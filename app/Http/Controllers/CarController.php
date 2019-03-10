b99f6d9d4c2633d7384a8eaca2549c93d16a1e7d
Jose Luis Ferrete <joseluisferrete@gmail.com>
2019-03-10 19:21:15 +0100

    Metodos CRUD

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -10,17 +10,22 @@ class CarController extends Controller
 {
     public function index(Request $request) {
 
-        $hash = $request->header('Authorization', null);
+        $cars = Car::all()->load('user');
 
-        $jwtAuth = new JwtAuth();
-        $checkToken = $jwtAuth->checkToken($hash);
+        return response()->json(array(
+            'cars' => $cars,
+            'status' => 'success'
+        ), 200);
 
-        if($checkToken){
-            echo "Index de CarController AUTENTICADO"; die();
-        }else{
-            echo "NO AUTENTICADO -> Index de CarController"; die();
-        }
+    }
 
+    public function show($id){
+
+        $car = Car::find($id)->load('user');
+        return response()->json(array(
+            'car' => $car,
+            'status' => 'success'
+        ), 200);
     }
 
     public function store(Request $request) {
@@ -81,6 +86,85 @@ class CarController extends Controller
 
         return response()->json($data, 200);
 
+    }
+
+    public function update($id, Request $request){
+
+        $hash = $request->header('Authorization', null);
+
+        $jwtAuth = new JwtAuth();
+        $checkToken = $jwtAuth->checkToken($hash);
+
+        if($checkToken){
+            //Recoger parametros POST
+            $json = $request->input('json', null);
+            $params = json_decode($json);
+            $params_array = json_decode($json, true);
+
+            //Validar datos
+            $validate= \Validator::make($params_array, [
+                'title' => 'required',
+                'description' => 'required',
+                'price' => 'required',
+                'status' => 'required'
+
+            ]);
+
+            if($validate->fails()){
+                return response()->json($validate->errors(), 400);
+            }
+
+            //Actualizar el registro
+            $car = Car::where('id', $id)->update($params_array);
+
+            $data = array(
+              'car' => $params,
+              'status' => 'success',
+              'code' => '200'
+            );
+
+
+        }else {
+            //Devolver error
+            $data = array(
+                'message' => 'Login incorrecto',
+                'status' => 'error',
+                'code' => 300,
+            );
+        }
+
+        return response()->json($data, 200);
+    }
+
+    public function destroy($id, Request $request){
+        $hash = $request->header('Authorization', null);
+
+        $jwtAuth = new JwtAuth();
+        $checkToken = $jwtAuth->checkToken($hash);
+
+        if($checkToken){
+            // Comprobar que existe el registro
+            $car = Car::find($id);
+
+            //Borrarlo
+            $car->delete();
+
+            //Devolverlo
+            $data = array(
+                'car' => $car,
+                'status' => 'success',
+                'code' => 200
+            );
+
+        }else{
+            $data = array(
+                'status' => 'error',
+                'code' => '400',
+                'message' => 'Login incorrecto !!'
+            );
+        }
+
+        return response()->json($data, 200);
     }
 
 }
