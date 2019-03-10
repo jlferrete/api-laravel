ee1dfa424334d45f574ae4b0f1e0a7cd8e2e928b
Jose Luis Ferrete <joseluisferrete@gmail.com>
2019-03-10 18:29:10 +0100

    Guardar y validar datos Servicio REST

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Helpers\JwtAuth;
+use App\Car;
 
 class CarController extends Controller
 {
@@ -21,4 +22,65 @@ class CarController extends Controller
         }
 
     }
+
+    public function store(Request $request) {
+
+        $hash = $request->header('Authorization', null);
+
+        $jwtAuth = new JwtAuth();
+        $checkToken = $jwtAuth->checkToken($hash);
+
+        if($checkToken){
+            //Recoger datos por POST
+            $json = $request->input('json', null);
+            $params = json_decode($json);
+            $params_array = json_decode($json, true);
+
+            //Conseguir el usuario identificado
+            $user = $jwtAuth->checkToken($hash, true);
+
+            //Validacion
+
+            $validate= \Validator::make($params_array, [
+                'title' => 'required',
+                'description' => 'required',
+                'price' => 'required',
+                'status' => 'required'
+
+            ]);
+
+            if($validate->fails()){
+                return response()->json($validate->errors(), 400);
+            }
+
+
+            //Guardar el coche
+            $car = new Car();
+            $car->user_id = $user->sub;
+            $car->title = $params->title;
+            $car->description = $params->description;
+            $car->price = $params->price;
+            $car->status = $params->status;
+
+            $car->save();
+
+            $data = array(
+                'car' => $car,
+                'status' => 'success',
+                'code' => 200,
+            );
+
+        }else{
+            //Devolver error
+            $data = array(
+                'message' => 'Login incorrecto',
+                'status' => 'error',
+                'code' => 300,
+            );
+        }
+
+        return response()->json($data, 200);
+
+    }
+
 }
